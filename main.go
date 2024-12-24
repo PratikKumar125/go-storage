99885632af9b2fd2b43ae3afca1549940b538fbe
PratikKumar125 <pratik.kumar@squareboat.com>
2024-12-24 22:34:28 +0530

    ADD:Initital commit

---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"go-storage/storage"
+	"io"
+	"net/http"
+	"os"
+	"text/template"
+	"time"
+
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+)
+
+var templates = template.Must(template.ParseFiles("public/upload.html"))
+
+func display(w http.ResponseWriter, page string, data interface{}) {
+	templates.ExecuteTemplate(w, page+".html", data)
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10 << 20)
+
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// Create file
+	dst, err := os.Create(handler.Filename); if err != nil {
+		panic(err)
+	}
+
+	defer dst.Close()
+
+	// Copy the uploaded file to the created file on the filesystem
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//compression
+
+	go transformLarge(handler.Filename)
+	go transformMedium(handler.Filename)
+	go transformSmall(handler.Filename)
+
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		display(w, "upload", nil)
+	case "POST":
+		uploadFile(w, r)
+	}
+}
+
+func transformLarge(path string) (error) {
+	ffmpeg.Input(path).Output("./regi_large.png",
+			ffmpeg.KwArgs{"vf": "scale=3840x2160:flags=lanczos"},
+		).
+		Run()
+	time.Sleep(10 * time.Second)
+	return nil
+}
+func transformMedium(path string) (error) {
+	ffmpeg.Input(path).Output("./regi_medium.png",
+	ffmpeg.KwArgs{"vf": "scale=1920x1080:flags=lanczos"},
+	).
+	Run()
+	time.Sleep(5 * time.Second)
+	return nil
+}
+
+func transformSmall(path string) (error) {
+	ffmpeg.Input(path).Output("./regi_small.png",
+			ffmpeg.KwArgs{"vf": "scale=1280x720:flags=lanczos"},
+		).
+		Run()
+	time.Sleep(3 * time.Second)
+	return nil
+}
+
+func main() {
+	originals := storage.DiskStruct{
+		Bucket: "testing-media-services",
+		Region: "us-east-1",
+		Profile: "ankush_prasoon_account",
+	}
+	medium := storage.DiskStruct{
+		Bucket: "pdf",
+		Region: "eu-north-1",
+		Profile: "chetaaaaaan",
+	}
+
+	disks := map[string]storage.DiskStruct{
+        "originals": originals,
+        "medium":   medium,
+    }
+
+	// Initializing the available disks and the current disks
+	st := storage.InitStorage("originals", disks)
+
+	items, err := st.GetBucketItems()
+	fmt.Println(items,err, "<<<<<<BUCKET ITEMS")
+
+	http.HandleFunc("/upload", uploadHandler)
+	http.ListenAndServe(":8080", nil)
+	
+	// url, err := st.SignedURL("/originals/testing3.mp3")
+	// fmt.Println(url, err)
+
+	// st.SetCurrentDisk("small")
+	// st.SetCurrentDisk("medium")
+}
---
+++ storage/s3.go
@@ -0,0 +1,177 @@
+package storage
+
+import (
+	"io"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+// [
+// 	"mediaDisk" : {
+// 		Bucket: "",
+// 		Region: "",
+// 		Profile: "",
+// 	},
+// 	"pdfDisk" : {
+// 		Bucket: "",
+// 		Region: "",
+// 		Profile: "",
+// 	},
+// ]
+
+type DiskStruct struct {
+	Bucket string
+	Region string
+	Profile string
+}
+
+type StorageStruct struct {
+	Disks map[string]DiskStruct
+	connection *s3.S3
+	CurrentDisk string
+}
+
+func InitStorage(currentDisk string, disks map[string]DiskStruct) *StorageStruct {
+	storage := &StorageStruct{
+		CurrentDisk: currentDisk,
+		Disks:    disks,
+	}
+	storage.SetCurrentDisk(currentDisk) 
+	return storage
+}
+
+func (st *StorageStruct) SetCurrentDisk(diskName string) {
+	if _, exists := st.Disks[diskName]; exists {
+		st.CurrentDisk = diskName
+		st.connection = nil
+	} else {
+		panic("No Disk found")
+	}
+}
+
+func (st *StorageStruct) InitConnection() {
+	if st.connection == nil {
+		disk := st.Disks[st.CurrentDisk]
+		sess, err := session.NewSession(&aws.Config{
+			Region:      aws.String(disk.Region),
+			Credentials: credentials.NewSharedCredentials("", disk.Profile),
+		})
+		if err != nil {
+			panic("Unable to create AWS session: " + err.Error())
+		}
+		st.connection = s3.New(sess)
+	}
+}
+
+func (st *StorageStruct) GetBucketItems() (s3.ListObjectsV2Output, error) {
+	st.InitConnection()
+	currentDiskInfo := st.getDiskInfo()
+
+	resp, err := st.connection.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(currentDiskInfo.Bucket)})
+	if err != nil {
+		panic(err)
+	}
+	return *resp, nil
+}
+
+func (st *StorageStruct) Put(path string, body io.Reader) (*s3manager.UploadOutput, error) {
+	st.InitConnection()
+	currentDiskInfo := st.getDiskInfo()
+
+	sess, err := session.NewSession(&aws.Config{
+    	Region: aws.String(currentDiskInfo.Region),
+		Credentials: credentials.NewSharedCredentials("", currentDiskInfo.Profile),
+	})
+	if err != nil {
+		return nil, err
+	}
+	uploader := s3manager.NewUploader(sess)
+
+	output, err := uploader.Upload(&s3manager.UploadInput{
+    	Bucket: aws.String(currentDiskInfo.Bucket),
+    	Key: aws.String(path),
+    	Body: body,
+	})
+	if err != nil {
+		return nil, err
+	}
+	
+	return output, nil
+}
+
+func (st *StorageStruct) Delete(path string) (bool, error) {
+	st.InitConnection()
+	currentDiskInfo := st.getDiskInfo()
+	
+	_, err := st.connection.DeleteObject(&s3.DeleteObjectInput{Bucket: aws.String(currentDiskInfo.Bucket), Key: aws.String(path)})
+
+	if err != nil {
+		return false, err
+	}
+
+	watchErr := st.connection.WaitUntilObjectNotExists(&s3.HeadObjectInput{
+		Bucket: aws.String(currentDiskInfo.Bucket),
+		Key:    aws.String(path),
+	})
+
+	if watchErr != nil {
+		return false, watchErr
+	}
+
+	return true, nil
+}
+
+func (st *StorageStruct) Exists(path string) (s3.HeadObjectOutput, error) {
+	st.InitConnection()
+	currentDiskInfo := st.getDiskInfo()
+
+	res, err := st.GetObjectHead(currentDiskInfo.Bucket, path)
+	if err != nil {
+		panic(err)
+	}
+	return res,nil
+}
+
+func (st *StorageStruct) SignedURL(path string) (string, error) {
+	st.InitConnection()
+	currentDiskInfo := st.getDiskInfo()
+
+	req, _ := st.connection.PutObjectRequest(&s3.PutObjectInput{
+        Bucket: aws.String(currentDiskInfo.Bucket),
+        Key:    aws.String(path),
+    })
+    url, err := req.Presign(15 * time.Minute)
+	return url, err
+}
+
+func (st *StorageStruct) Meta(path string) (s3.HeadObjectOutput, error) {
+	st.InitConnection()
+	currentDiskInfo := st.getDiskInfo()
+
+	res, err := st.GetObjectHead(currentDiskInfo.Bucket, path)
+	if err != nil {
+		panic(err)
+	}
+	return res,nil
+}
+
+func (st *StorageStruct) GetObjectHead(bucket string, path string) (s3.HeadObjectOutput, error) {
+	st.InitConnection()
+	output, err := st.connection.HeadObject(&s3.HeadObjectInput{
+		Bucket: &bucket,
+		Key: &path,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return *output, nil
+}
+
+func (st *StorageStruct) getDiskInfo() DiskStruct{
+	return st.Disks[st.CurrentDisk]
+}
